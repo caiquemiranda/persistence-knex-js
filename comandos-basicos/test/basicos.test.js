6b4529000efbc74279211437caea9af301aa24c5
caiquemiranda <caiquemirandas@gmail.com>
2025-07-14 21:16:33 -0300

    test createTable

--- comandos-basicos/test/basicos.test.js
+++ comandos-basicos/test/basicos.test.js
@@ -3,6 +3,8 @@ const knex = require("knex")(knexfile)
 
 const criarTabela = require("../src/criarTabela")
 const excluirTabela = require("../src/deletarTabela")
+const inserirNaTabela1 = require("../src/inserirNaTabela")
+
 
 test("deve criar tabela", async ()=>{
     await criarTabela(knex)
@@ -14,6 +16,13 @@ test("deve excluir tabela", async()=>{
     await excluirTabela(knex)
     const tabelaExiste = await knex.schema.hasTable("livros")
     expect(tabelaExiste).toBe(false)
+})
+
+test("deve inserir conteudo na tabela", async ()=>{
+    const dados = await inserirNaTabela1(knex)
+    console.log(dados)
+    expect(dados).toBeInstanceOf(Array)
+    expect(dados).toHaveLength(1)
 })
 
 afterAll(() =>{
